c4b8d4be98afeb171b352189feebce173dcdba5d
Nabin Ghimire <nabin@bentraytech.com>
2023-12-22 09:25:19 +0545

    front end changes

--- Ecommerce/Ecommerce/obj/Debug/net7.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net7.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f475e6920897241ba43118dcf04c365f6ad8226a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
